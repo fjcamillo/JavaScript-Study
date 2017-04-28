8847efb586fc16cd08d81594dfa6c3d0c2d1908d
fjcamillo <fjcamillo.dev@gmail.com>
2017-04-28 09:39:03 +0800

    starting es6

---
+++ ES6/1-Start.js
@@ -0,0 +1,11 @@
+var a = 2;
+{
+  let a = 3
+  console.log(a); //This was the first one printed
+
+
+}
+console.log(a); //second one printed
+
+//learning
+//let is block scoped and var is function scope
